d0632669cde3ef3c674c8c55564964ff9be83a66
LMontoya <66293930+BlackDeath12@users.noreply.github.com>
2024-04-20 23:03:12 -0700

    Update objectRec.py

--- Assets/objectRec.py
+++ Assets/objectRec.py
@@ -1,5 +1,3 @@
-import pathlib
-import textwrap
 import sys
 
 import google.generativeai as genai
@@ -9,18 +7,10 @@ from IPython.display import Markdown
 
 import PIL.Image
 
-def returnString(result):
-  return str(result)
-
 if __name__ == "__main__":
   file = sys.argv[1]
 
   img = PIL.Image.open(file)
-
-  def to_markdown(text):
-    text = text.replace('•', '  *')
-    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
 
   GOOGLE_API_KEY = 'AIzaSyCmEqw2hJ-mOK6U479aiGSRbsPp1xur_f0'
 
@@ -31,4 +21,3 @@ if __name__ == "__main__":
   response = model.generate_content(["Tell me what object is this. Keep it in a single word.", img], stream=True)
   response.resolve()
   print(response.text)
-  returnString(response.text)
